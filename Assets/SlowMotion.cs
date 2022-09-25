b0acef4ae289fc1432cee1c543270efee9626725
Billu <Billowski98@gmail.com>
2022-09-25 09:35:27 +0200

    Selecting objects in 3D

---
+++ Assets/SlowMotion.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMotion : MonoBehaviour
+{
+    public float slowMotionTimeScale;
+
+    private float startTimeScale;
+    private float startFixedDeltaTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTimeScale = Time.timeScale;
+        startFixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartSlowMotion();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StopSlowMotion();
+        }
+    }
+
+    private void StartSlowMotion()
+    {
+        Time.timeScale = slowMotionTimeScale;
+        Time.fixedDeltaTime = startFixedDeltaTime * slowMotionTimeScale;
+    }
+
+    private void StopSlowMotion()
+    {
+        Time.timeScale = startTimeScale;
+        Time.fixedDeltaTime = startFixedDeltaTime;
+    }
+}
